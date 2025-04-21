bd2a714d90fa20bcd7f3bcaed0596b6ed71ac53b
Callista Holleschak <nachboss@DN0a1e281d.SUNet>
2025-04-20 21:33:40 -0700

    Hello

--- main.py
+++ main.py
@@ -18,20 +18,17 @@ g = 9.81  # [m/s^2]
 mass = 71000  # [kg]
 weight = mass * g
 cruise_mach = 0.8  # [mach]
-cruise_alt = 36000  # [ft]
-service_ceil = 43000  # [ft]
+cruise_alt = 11000  # [m]
+service_ceil = 13100  # [m]
 
 # --- Propulsion ---
-Engine_thrust = 105000  # [N] for typical midsize plane engine
-Engine_fuel_consumption = 0.0035  # [kg/m] for typical midsize plane engine
+engine_thrust = 105000  # [N] for typical midsize plane engine
+engine_tsfc = 0.0035  # [kg/m] for typical midsize plane engine
 
 # ---------- ATMOSPHERIC MODEL ----------
-atm = asb.atmosphere.Atmosphere(cruise_alt * 0.3048)
+atm = asb.atmosphere.Atmosphere(cruise_alt)
 cruise_speed = atm.speed_of_sound() * cruise_mach
 viscosity = atm.dynamic_viscosity()
-
-
-
 
 # ---------- OPTIMIZATION MODEL ----------
 opti=asb.Opti(cache_filename="output/soln1.json")
@@ -39,25 +36,15 @@ opti=asb.Opti(cache_filename="output/soln1.json")
 # ---------- VARIABLES ----------
 # --- Aerodynamic ---
 wing_airfoil = asb.Airfoil("sc20412") #TODO: Replace with optimized airfoil from above
-wingspan = opti.variable(init_guess=250, lower_bound=230, upper_bound=265, scale=4)
-chordlen = opti.variable(init_guess=12, lower_bound=8, upper_bound = 15, scale=2)
-aoa = opti.variable(init_guess=2, lower_bound=-5, upper_bound= 10, scale=1)
-ar = opti.variable(init_guess=20, lower_bound=18, upper_bound=30, scale=1)
-di_range = opti.variable(init_guess=2, lower_bound=0, upper_bound=10, scale=1)
+wingspan = opti.variable(init_guess=45, lower_bound=35, upper_bound=50, scale=10)
+chordlen = opti.variable(init_guess=7, lower_bound=6, upper_bound=8, scale=2)
+aoa = opti.variable(init_guess=2, lower_bound=-5, upper_bound=10, scale=1)
+wing_area = chordlen * wingspan
+ar = wingspan**2 / wing_area
 sweep = opti.variable(init_guess=15, lower_bound=5, upper_bound=30, scale=2)
-
-# --- Structural ---
-wing_thickness = opti.variable(init_guess=0.2, lower_bound=.1, upper_bound=.5, scale=1)
-struct_defined_aoa = opti.variable(init_guess=2, lower_bound=-1, upper_bound=5, scale=1)
-cg_le_dist = opti.variable(init_guess=0.05, lower_bound=0, scale=1)
-
-
-
 
 q_inf = (atm.density() * (cruise_speed ** 2)) / 2
 reynolds_num = (atm.density() * (cruise_speed) * wingspan) / viscosity
-
-
 
 # ---------- GEOMETRIES ----------
 main_wing = asb.Wing(
@@ -65,7 +52,6 @@ main_wing = asb.Wing(
     symmetric=True,  # Should this wing be mirrored across the XZ plane?
     span=wingspan,  # Span of the wing
     aspect_ratio=ar,  # Aspect ratio of the wing
-    dihedral=di_range,  # Dihedral angle of the wing in degrees
     mean_sweep_angle=sweep,
 
     xsecs=[  # The wing's cross ("X") sections
@@ -76,38 +62,22 @@ main_wing = asb.Wing(
                 0,
             ],  # Coordinates of the XSec's leading edge, relative to the wing's leading edge.
             chord=chordlen,
-            twist=struct_defined_aoa,  # degrees
+            twist=aoa,  # degrees
             airfoil=wing_airfoil,  # Airfoils are blended between a given XSec and the next one.
 
         ),
-        asb.WingXSec(  # Mid
-            xyz_le=[0.00, 0.5 * wingspan / 2, 0],
-            chord=chordlen * 0.75,  # Mid chord is ~75% of root chord
-            twist=struct_defined_aoa,
-            airfoil=wing_airfoil,
-        ),
         asb.WingXSec(  # Tip
-            xyz_le=[0.00, wingspan / 2, np.sin(10 * np.pi / 180) * 0.5 * wingspan / 2],
-            chord=chordlen * .2,  # Tip chord is 20% of root chord
-            twist=struct_defined_aoa,
+            xyz_le=[
+                0,
+                wingspan,
+                0,
+            ],
+            chord=chordlen,  # Tip chord is 20% of root chord
+            twist=aoa,
             airfoil=wing_airfoil,
         ),
     ],
 )
-
-main_fuselage = asb.Fuselage(  # main fuselage
-    name="Fuselage",
-    xsecs=[
-        asb.FuselageXSec(
-            xyz_c=[0.5 * xi, 0, 0],
-            radius=3.76
-                   * asb.Airfoil("sc20412").local_thickness(
-                x_over_c=xi
-            ),  # half a meter fuselage. Starting at LE and 0.5m forward
-        )
-        for xi in np.cosspace(0, 1, 30)
-    ],
-).translate([-0.5, 0, 0])
 
 ### Define the 3D geometry you want to analyze/optimize.
 # Here, all distances are in meters and all angles are in degrees.
@@ -115,9 +85,8 @@ airplane = asb.Airplane(
     name="ACE-1",
     xyz_ref=[0.25 * chordlen, 0, 0],  # CG location
     wings=[main_wing],
-    # fuselages=[main_fuselage],
 )
-[63]
+
 # ---------- AERODYNAMICS ----------
 vlm = asb.VortexLatticeMethod(
     airplane=airplane,
@@ -131,30 +100,35 @@ aero = vlm.run_with_stability_derivatives()  # Returns a dictionary
 # ---------- CONSTRAINTS ----------
 # --- Aerodynamic ---
 opti.subject_to(aero["L"] == weight)
-opti.subject_to(wing_airfoil.max_thickness() * chordlen > .05)  # must accomodate main spar (22mm)
-opti.subject_to(main_wing.aspect_ratio() > 20)  # Aspect ratio must match
-opti.subject_to(main_wing.mean_dihedral_angle() > 0)  # Dihedral must be positive
+opti.subject_to(main_wing.aspect_ratio() > 15)  # Aspect ratio must match
 opti.subject_to(main_wing.mean_sweep_angle() > 5)  # Sweep must be greater than 5 degrees
+opti.subject_to(wing_area >= 100)
+opti.subject_to(wing_area <= 400)
+opti.subject_to(ar <= 30)
 
 # ---------- SOLVE ----------
+
 opti.minimize(wingspan)
-sol = opti.solve()
-opti.save_solution()
+try:
+    sol = opti.solve()
+    opti.save_solution()
 
-print("Wingspan:", sol(wingspan))
-print("Chordlen:", sol(chordlen))
-print("Aspect Ratio:", sol(ar))
-print("Dihedral:", sol(di_range))
-print("Sweep:", sol(sweep))
-print("Wing Thickness:", sol(wing_thickness))
-print("AoA:", sol(aoa))
-print("Structural AoA:", sol(struct_defined_aoa))
+    print("Wingspan:", sol(wingspan))
+    print("Chordlen:", sol(chordlen))
+    print("Aspect Ratio:", sol(ar))
+    print("Sweep:", sol(sweep))
+    print("AoA:", sol(aoa))
 
-for k, v in aero.items():
-    print(f"{k.rjust(4)} : {sol(aero[k])}")
+    for k, v in aero.items():
+        print(f"{k.rjust(4)} : {sol(aero[k])}")
 
-vlm=sol(vlm)
-vlm.draw()
+    vlm=sol(vlm)
+    vlm.draw()
 
-airplane=sol(airplane)
-airplane.draw()
+    airplane=sol(airplane)
+    airplane.draw()
+
+except RuntimeError as e:
+    opti.debug.show_infeasibilities()
+    sol=opti.debug
+    print(sol)
