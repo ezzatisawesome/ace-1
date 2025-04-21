beb951f3b8596254816fe406914f330860df3ff5
calskye <callista@holleschak.com>
2025-04-21 12:34:38 -0700

    sg

--- new.py
+++ new.py
@@ -11,10 +11,10 @@ g = 9.81  # [m/s^2]
 e = 0.7  # Oswald efficiency factor standard for A320
 
 # Structural
-mass = 50000  # [kg]
+mass = 63000  # [kg]
 weight = mass * g
 weight_fuselage = 20000 * g  # [N] assumed fuselage weight
-W_W_coeff1 = 4.71e-5  # Wing Weight Coefficient 1 [1/m]
+W_W_coeff1 = 7e-6  # Wing Weight Coefficient 1 [1/m]
 W_W_coeff2 = 45.24  # Wing Weight Coefficient 2 [Pa]
 ultimate_load_factor = 3.8  # ultimate load factor [-]
 airfoil_thickness_fraction = 0.12  # Approx. for SC(2)-0412
@@ -80,8 +80,8 @@ truss_wing = asb.Wing(
         ),
         asb.WingXSec(
             xyz_le=[
-                (span / 4) * 0.6,  # A bit inward from wing tip
-                span / 4,
+                4,  # A bit inward from wing tip
+                span / 5,
                 -0.1  # Slight vertical offset to angle the truss
             ],
             chord=0.5,
